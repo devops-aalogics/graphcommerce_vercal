94e5fff8ba59736f3f8bbcd581bbf8df16f760d3
aliasif <aliasifmse.asif@gmail.com>
2024-04-06 14:25:09 +0500

    changing domain name

--- graphcommerce.config.js
+++ graphcommerce.config.js
@@ -8,10 +8,10 @@
 const config = {
   hygraphEndpoint: 'https://eu-central-1.cdn.hygraph.com/content/ckhx7xadya6xs01yxdujt8i80/master',
   magentoEndpoint: 'https://staging.richy.sa/graphql',
-  canonicalBaseUrl: 'https://graphcommerce.vercel.app',
+  canonicalBaseUrl: 'https://headless.richy.sa',
   storefront: [
-    { locale: 'en', magentoStoreCode: 'en', defaultLocale: true },
-    { locale: 'ar', magentoStoreCode: 'ar' },
+    { locale: 'en', magentoStoreCode: 'en' },
+    { locale: 'ar', magentoStoreCode: 'ar', defaultLocale: true },
   ],
   recentlyViewedProducts: {
     enabled: true,
