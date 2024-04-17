1a58d5aafae5a49d813d3587a34d016f78433747
aliasif <aliasifmse.asif@gmail.com>
2024-04-17 13:22:59 +0500

    changing hygraph endpoint

--- graphcommerce.config.js
+++ graphcommerce.config.js
@@ -6,7 +6,7 @@
  * @type {import('@graphcommerce/next-config/src/generated/config').GraphCommerceConfig}
  */
 const config = {
-  hygraphEndpoint: 'https://api-eu-west-2.hygraph.com/v2/clv1yiz1z000008l8hsh91l2j/master',
+  hygraphEndpoint: 'https://eu-west-2.cdn.hygraph.com/content/clv1yiz1z000008l8hsh91l2j/master',
   magentoEndpoint: 'https://staging.richy.sa/graphql',
   canonicalBaseUrl: 'https://headless.richy.sa',
   storefront: [
